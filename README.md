9d35ef1b5aa903c85a3d70f8b4c3868e1ed09033
Prateek Kumar <cs15btech11031@iith.ac.in>
2016-07-19 22:49:04 +0530

    update getDoc, createDoc, createDB, deleteDB, getAllDocs functions in index.js

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ $ npm install couchdbjs --save
 ```
 
 ### Create a couchdbjs object
-```javascript
+```js
 const Couchdbjs = require('couchdbjs');
 const db = new Couchdbjs(dbname, options);
 ```
 Note : Here DB is the object of the class couchdbjs.
 
-Here `options` is an optional javascript object containing the configuration.
+Here `options` is an optional js object containing the configuration.
 This is the default configuration.
-```javascript
+```js
 {
     protocol : 'http:',
     hostname: 'localhost',
@@ -26,12 +26,12 @@ This is the default configuration.
 `dbname` is the name of the database.
 
 ### Generate a new uuid
-```
+```js
 Couchdbjs.getNewId(options, cb);
 ```
 Here `options` is optional configuration object.
 This is the default configuration.
-```javascript
+```js
 {
     protocol : 'http:',
     hostname : 'localhost',
@@ -41,11 +41,11 @@ This is the default configuration.
 ```
 `cb` is reqired callback function with two parameters `err` and `data`.
 To avoid writing configurations for protocol, hostname, etc. again the function `getConfig` can be called on existing database object.
-```javascript
+```js
 db.getConfig({count : 1});
 ```
 returns
-```javascript
+```js
 {
     protocol : 'http:',
     hostname : 'localhost',
@@ -54,9 +54,79 @@ returns
 }
 ```
 Complete Example :
-```javascript
+```js
 Couchdbjs.getNewId(db.getConfig({count : 1}, function(err, data) {
     if (err) console.error(err);
     else console.log(data[0]); // data is array of uuids
 }));
+```
+
+### Create a new database
+```js
+const db = new Couchdbjs('dbname', options);
+```
+This creates a database when a database with name `dbname` doesn't exist.
+Database can also be created using `Couchdbjs.createDB` function.
+```js
+Couchdbjs.createDB({
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: 5984,
+    db: 'dbanme'
+}, function(err, data)=>{
+    if (err) console.error(err);
+    else console.log(data);
+});
+```
+
+### Delete a database
+Database can be deleted using function `Couchdbjs.deleteDB` similar to `Couchdbjs.createDB`.
+```js
+Couchdbjs.deleteDB({
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: 5984,
+    db: 'dbanme'
+}, function(err, data) {
+    if (err) console.error(err);
+    else console.log(data);
+});
+
+```
+
+### List all documents in a database
+```js
+db.getAllDocs(function(err, data) {
+    if (err) console.error(err);
+    else console.log(data);
+});
+```
+
+### Create a new document
+```js
+db.createDoc(id, document, cb);
+```
+`id` is the id of the document to be used.
+`document` is the object to be stored in the document.
+`cb` is the callback function with parameters `err` and `data`.
+Example
+```js
+db.createDoc('id_doc', {a:1, b:2, c:[5, 'k']}, function(err, data) {
+    if (err) console.error(err);
+    else console.log(data);
+});
+```
+
+### Get document
+```js
+db.deleteDoc(id, cb);
+```
+`id` is the id of the document.
+`cb` is the callback function with parameters `err` and `data`.
+Example
+```js
+db.getDoc('id_doc', function(err, data) {
+    if (err) console.error(err);
+    else console.log(data);
+});
 ```
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const request = require('request');
 const url = require('url');
 const colors = require('colors');
+const stream = require('stream');
 
 class DBHandler {
     constructor(db, options) {
@@ -26,7 +27,7 @@ class DBHandler {
             port: this.config.port,
             pathname: '/'+encodeURIComponent(this.db)
         });
-        request({url : uri}, (err, res, body)=>{
+        request(uri, (err, res, body)=>{
             if (err) cb(err);
             else if (res.statusCode == 200) {
                 console.log(`Conected to database successfully at ${uri.green}`);
@@ -40,6 +41,58 @@ class DBHandler {
                         cb(null);
                     }
                 });
+            }
+        });
+    }
+
+    static createDB(options, cb) {
+        if (!cb) cb = ()=>{};
+        let config = {
+            protocol: 'http:',
+            hostname: 'localhost',
+            port: 5984,
+        };
+        for (var attr in options) {
+            config[attr] = options[attr];
+        }
+        let uri = url.format({
+            protocol : config.protocol,
+            hostname : config.hostname,
+            port : config.port,
+            pathname : '/'+encodeURIComponent(config.db)
+        });
+        request.put(uri, (err, res, body)=>{
+            if (err) cb(err);
+            else {
+                let data = JSON.parse(body);
+                if (data.error) cb(data);
+                else cb(null, data);
+            }
+        });
+    }
+
+    static deleteDB(options, cb) {
+        if (!cb) cb = ()=>{};
+        let config = {
+            protocol: 'http:',
+            hostname: 'localhost',
+            port: 5984,
+        };
+        for (var attr in options) {
+            config[attr] = options[attr];
+        }
+        let uri = url.format({
+            protocol : config.protocol,
+            hostname : config.hostname,
+            port : config.port,
+            pathname : '/'+encodeURIComponent(config.db)
+        });
+        request.delete(uri, (err, res, body)=>{
+            if (err) cb(err);
+            else {
+                let data = JSON.parse(body);
+                if (data.error) cb(data);
+                else cb(null, data);
             }
         });
     }
@@ -82,191 +135,151 @@ class DBHandler {
                 count : config.count
             }
         });
-        request({url : uri}, (err, res, body)=>{
+        request(uri, (err, res, body)=>{
             if (err) cb(err);
-            else if (res.statusCode == 200) {
+            else {
                 let data = JSON.parse(body);
                 if (data.error) cb(data);
                 else cb(null, data.uuids);
             }
+        });
+    }
+
+    createDoc(id, doc, cb) {
+        if (!cb) cb = ()=>{};
+        let uri = url.format({
+            protocol : this.config.protocol,
+            hostname : this.config.hostname,
+            port : this.config.port,
+            pathname : '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
+        });
+        const str = new stream.Readable();
+        str.push(JSON.stringify(doc));
+        str.push(null);
+        str.pipe(request.put(uri, (err, res, body)=>{
+            if (err) cb(err);
             else {
-                cb(new Error('Error occurred while connecting to couchdb.'));
+                let data = JSON.parse(body);
+                if (data.error) cb(data);
+                else cb(null, data);
             }
-        });
+        }));
     }
 
-    getDoc(id, callback) {
-        if (!callback) callback = console.log;
-        http.request({
-            hostname: this.config.hostname,
-            port: this.config.port,
-            method: 'GET',
-            path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
-        }, (res)=>{
-            let data = '';
-            res.on('data', (chunk)=>{ data+=chunk; });
-            res.on('end', ()=>{
-                data = JSON.parse(data);
-                if(!data.error) {
-                    callback(false, data);
-                } else callback(data);
-            });
-        }).end();
-    }
-
-    getAllDocs(callback) {
-        if (!callback) callback = console.log;
-        http.request({
-            hostname: this.config.hostname,
-            port: this.config.port,
-            method: 'GET',
-            path: '/'+encodeURIComponent(this.db)+'/_all_docs'
-        }, (res)=>{
-            let data = '';
-            res.on('data', (chunk)=>{ data+=chunk; });
-            res.on('end', ()=>{
-                data = JSON.parse(data);
-                if(!data.error) {
-                    callback(false, data);
-                } else callback(data);
-            });
-        }).end();
-    }
-
-    createDoc(id, doc, callback) {
-        if (!callback) callback = console.log;
-        let req = http.request({
-            hostname: this.config.hostname,
-            port: this.config.port,
-            method: 'PUT',
-            path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
-        }, (res)=>{
-            let data = '';
-            res.on('data', (chunk)=>{ data+=chunk; });
-            res.on('end', ()=>{
-                data = JSON.parse(data);
-                console.log(data);
-                if (!data.error) {
-                    callback(false, data);
-                } else callback(data);
-            });
-        });
-        req.write(JSON.stringify(doc));
-        req.end();
-    }
-
-    updateDoc(id, doc, callback) {
-        if (!callback) callback = console.log;
-        let req = http.request({
-            hostname: this.config.hostname,
-            port: this.config.port,
-            method: 'PUT',
-            path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
-        }, (res)=>{
-            let data = '';
-            res.on('data', (chunk)=>{ data+=chunk; });
-            res.on('end', ()=>{
-                data = JSON.parse(data);
-                if(!data.error) {
-                    callback(false, data);
-                } else callback(data);
-            });
-        });
-        req.write(JSON.stringify(doc));
-        req.end();
-    }
-
-    attachFileInDoc(id, rev, file, callback) {
-        if(!callback) callback = console.log;
-        var fstream = fs.createReadStream(file.path);
-        var path = '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)+'/'+file.name;
-        var u = url.format({
+    getDoc(id, cb) {
+        if (!cb) cb = ()=>{};
+        let uri = url.format({
             protocol: this.config.protocol,
-            hostname: this.config.hostname,
-            port: this.config.port,
-            pathname: path,
-            query: {rev: rev}
+            hostname : this.config.hostname,
+            port : this.config.port,
+            pathname : '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
         });
-        fstream.pipe(request.put({
-            url: u,
-            headers: {
-                'Content-type': file.mimetype
+        request(uri, (err, res, body)=>{
+            if (err) cb(err);
+            else {
+                let data = JSON.parse(body);
+                if (data.error) cb(data);
+                else cb(null, data);
             }
-        }, callback));
+        });
     }
 
-    deleteDoc(id, callback) {
-        if (!callback) callback = console.log;
-        http.request({
-            hostname: this.config.hostname,
-            port: this.config.port,
-            method: 'GET',
-            path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
-        }, (res)=>{
-            let data = '';
-            res.on('data', (chunk)=>{ data+=chunk; });
-            res.on('end', ()=>{
-                data = JSON.parse(data);
-                if(!data.error) {
-                    http.request({
-                        hostname: this.config.hostname,
-                        port: this.config.port,
-                        method: 'DELETE',
-                        path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)+'?rev='+data["_rev"]
-                    }, (res)=>{
-                        let data = '';
-                        res.on('data', (chunk)=>{ data+=chunk; });
-                        res.on('end', ()=>{
-                            data = JSON.parse(data);
-                            if(!data.error) {
-                                callback(false);
-                            } else callback(data);
-                        });
-                    }).end();
-                } else callback(data);
-            });
-        }).end();
+    getAllDocs(cb) {
+        if (!cb) cb = ()=>{};
+        let uri = url.format({
+            protocol: this.config.protocol,
+            hostname : this.config.hostname,
+            port : this.config.port,
+            pathname : '/'+encodeURIComponent(this.db)+'/_all_docs'
+        });
+        request(uri, (err, res, body)=>{
+            if (err) cb(err);
+            else {
+                let data = JSON.parse(body);
+                if (data.error) cb(data);
+                else cb(null, data);
+            }
+        });
     }
 
-    static createDB(options, dbName, callback) {
-        if (!callback) callback = console.log;
-        http.request({
-            hostname: options.hostname,
-            port: options.port,
-            method: 'PUT',
-            path: '/'+encodeURIComponent(dbName)
-        }, (res)=>{
-            let data = '';
-            res.on("data", (chunk)=>{
-                data += chunk;
-            });
-            res.on("end", ()=>{
-                data = JSON.parse(data);
-                if (data.ok) callback(false);
-                else callback(data);
-            });
-        }).end();
-    }
 
-    static deleteDB(options, dbName, callback) {
-        if (!callback) callback = console.log;
-        http.request({
-            hostname: options.hostname,
-            port: options.port,
-            method: 'DELETE',
-            path: '/'+encodeURIComponent(dbName)
-        }, (res)=>{
-            let data = '';
-            res.on('data', (chunk)=>{
-                data += chunk;
-            });
-            res.on('end' ,()=>{
-                data=JSON.parse(data);
-                if(!data.error) callback(null);
-                else callback(data);
-            })
-        }).end();
-    }
+    // 
+    // updateDoc(id, doc, callback) {
+    //     if (!callback) callback = console.log;
+    //     let req = http.request({
+    //         hostname: this.config.hostname,
+    //         port: this.config.port,
+    //         method: 'PUT',
+    //         path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
+    //     }, (res)=>{
+    //         let data = '';
+    //         res.on('data', (chunk)=>{ data+=chunk; });
+    //         res.on('end', ()=>{
+    //             data = JSON.parse(data);
+    //             if(!data.error) {
+    //                 callback(false, data);
+    //             } else callback(data);
+    //         });
+    //     });
+    //     req.write(JSON.stringify(doc));
+    //     req.end();
+    // }
+    //
+    // attachFileInDoc(id, rev, file, callback) {
+    //     if(!callback) callback = console.log;
+    //     var fstream = fs.createReadStream(file.path);
+    //     var path = '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)+'/'+file.name;
+    //     var u = url.format({
+    //         protocol: this.config.protocol,
+    //         hostname: this.config.hostname,
+    //         port: this.config.port,
+    //         pathname: path,
+    //         query: {rev: rev}
+    //     });
+    //     fstream.pipe(request.put({
+    //         url: u,
+    //         headers: {
+    //             'Content-type': file.mimetype
+    //         }
+    //     }, callback));
+    // }
+    //
+    // deleteDoc(id, callback) {
+    //     if (!callback) callback = console.log;
+    //     http.request({
+    //         hostname: this.config.hostname,
+    //         port: this.config.port,
+    //         method: 'GET',
+    //         path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)
+    //     }, (res)=>{
+    //         let data = '';
+    //         res.on('data', (chunk)=>{ data+=chunk; });
+    //         res.on('end', ()=>{
+    //             data = JSON.parse(data);
+    //             if(!data.error) {
+    //                 http.request({
+    //                     hostname: this.config.hostname,
+    //                     port: this.config.port,
+    //                     method: 'DELETE',
+    //                     path: '/'+encodeURIComponent(this.db)+'/'+encodeURIComponent(id)+'?rev='+data["_rev"]
+    //                 }, (res)=>{
+    //                     let data = '';
+    //                     res.on('data', (chunk)=>{ data+=chunk; });
+    //                     res.on('end', ()=>{
+    //                         data = JSON.parse(data);
+    //                         if(!data.error) {
+    //                             callback(false);
+    //                         } else callback(data);
+    //                     });
+    //                 }).end();
+    //             } else callback(data);
+    //         });
+    //     }).end();
+    // }
+    //
+    //
+
+
 };
 
 module.exports = DBHandler;
