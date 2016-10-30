471899cb81db9109f2b26fc91e772adc65144d9b
Prateek Kumar <pksingh1023@gmail.com>
2016-10-30 15:17:29 +0530

    Add docs/ folder
    Signed-off-by: Prateek Kumar <pksingh1023@gmail.com>

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,develop:0,index:0,modul:0,now:0,page:0,readme:0,refer:0,search:0,still:0,under:0},titles:["Welcome to couchdbjs&#8217;s documentation!"],titleterms:{couchdbj:0,document:0,indice:0,tabl:0,welcom:0}})
